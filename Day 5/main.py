b9448923af3cac52a2a1d2eb8a7542b6098f9757
Sob0r2 <jakubsoborski0@gmail.com>
2023-12-06 12:40:04 +0100

    Day 5 IT'S FAAAAST

--- Day 5/main.py
+++ Day 5/main.py
@@ -1,7 +1,7 @@
 from itertools import chain
 from concurrent.futures import ThreadPoolExecutor
 
-def map_to_location():
+def proccesing():
     file = open("input.txt").read().split('\n')
     seeds = file[0].split(' ')[1:]
     mapping_list = []
@@ -14,7 +14,10 @@ def map_to_location():
         else:
             tmp.append(line.split(' '))
     mapping_list.append(tmp)
+    return seeds,mapping_list
+def map_to_location():
 
+    seeds,mapping_list = proccesing()
     for mapping in mapping_list:
         for i,seed in enumerate(seeds):
             seed = int(seed)
@@ -25,37 +28,42 @@ def map_to_location():
     return(min(seeds))
 
 def map_to_location_2():
-    file = open("input.txt").read().split('\n')
+    seeds, mapping_list = proccesing()
+    seeds = [(int(seed[0]), int(seed[0]) + int(seed[1]) - 1) for seed in zip(seeds[::2], seeds[1::2])]
+    mapping_list = [[(int(mapp[0]), int(mapp[1]), int(mapp[1]) + int(mapp[2]) - 1) for mapp in mapping] for mapping in
+                    mapping_list]
 
-    seeds = file[0].split(' ')[1:]
+    for mapping in mapping_list:
+        new_res = []
+        for (ss, se) in seeds:
+            act = []
+            prev = []
+            for (mm, ms, me) in mapping:
+                dif = mm - ms
+                st = max(ss, ms)
+                lst = min(se, me)
+                if st <= lst:
+                    act.append((st + dif, lst + dif))
+                    prev.append((st, lst))
 
-    mapping_list = []
-    tmp = []
-    for line in file[3:]:
-        if line == '': continue
-        if ':' in line:
-            mapping_list.append(tmp)
-            tmp = []
-        else:
-            tmp.append(line.split(' '))
-    mapping_list.append(tmp)
+            if prev:
+                prev = sorted(prev, key=lambda x: x[0])
+                if prev[0][0] > ss:
+                    act.append((ss, prev[0][0] - 1))
+                for i in range(len(prev) - 1):
+                    if prev[i][1] + 1 != prev[i + 1][0]:
+                        act.append((prev[i][1] + 1, prev[i + 1][0] - 1))
+                if prev[-1][1] < se:
+                    act.append((prev[-1][1] + 1, se))
 
-    i = 10000000
-    mapping_list = list(reversed(mapping_list))
-    while True:
-        tmp = i
-        for line in mapping_list:
-            x = tmp
-            for val in line:
-                val = [int(v) for v in val]
-                if val[0] <= x < val[0]+val[2]:
-                    tmp = tmp - val[0] + val[1]
-        for seed in zip(seeds[::2],seeds[1::2]):
-            seed = [int(v) for v in seed]
-            if seed[0] <= tmp < seed[0] + seed[1]:
-                return i
-        i += 1
-        if i % 100000 == 0:
-            print(i)
+            else:
+                act.append((ss, se))
+            new_res.append(act)
+
+        seeds = list(chain(*new_res))
+
+
+    return min(seeds, key=lambda x: x[0])[0]
+
 
 print(map_to_location_2())

86d2a1616a51c7558c29d8055acc83c20247676a
Sob0r2 <jakubsoborski0@gmail.com>
2023-12-10 18:33:03 +0100

    Day 10

---
+++ Day 10/main.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+directions = {'left': (0, -1), 'right': (0, 1), 'up': (-1, 0), 'down': (1, 0)}
+pipe_directions = {
+    ('L', 'left'): 'up', ('F', 'up'): 'right', ('7', 'right'): 'down',
+    ('J', 'down'): 'left', ('F', 'left'): 'down',
+    ('7', 'up'): 'left', ('J', 'right'): 'up', ('L', 'down'): 'right',
+    ('-', 'left'): 'left', ('-', 'right'): 'right', ('|', 'up'): 'up', ('|', 'down'): 'down'
+}
+
+def first(pipes_arr, sx, sy):
+    if pipes_arr[sx, sy + 1] in ['-', 'J', '7']:
+        return (sx, sy + 1), 'right'
+    if pipes_arr[sx - 1, sy] in ['|', 'F', '7']:
+        return (sx - 1, sy), 'up'
+    if pipes_arr[sx + 1, sy] in ['|', 'J', 'L']:
+        return (sx + 1, sy), 'down'
+    if pipes_arr[sx, sy - 1] in ['-', 'F', 'L']:
+        return (sx, sy - 1), 'left'
+
+def ex1():
+    file_content = open("input.txt").read()
+    mat = [list(text) for text in file_content.split('\n')]
+    path = dfs(mat)
+    return len(path)//2
+
+def ex2():
+    file_content = open("input.txt").read()
+    mat = [list(text) for text in file_content.split('\n')]
+    path = dfs(mat)
+    res = set()
+    start = None
+    act_dir = ""
+    for i in range(len(mat)):
+        for j in range(len(mat[0])):
+            if (i,j) in path:
+                if path[(i,j)][1] == 'up' or pipe_directions[path[(i,j)]] == 'up':
+                    act_dir = 'up'
+                elif path[(i,j)][1] == 'down' or pipe_directions[path[(i,j)]] == 'down':
+                    act_dir = 'down'
+                if start is None:
+                    start = act_dir
+            elif start == act_dir:
+                res.add((i,j))
+    return len(res)
+
+def dfs(mat):
+    sx, sy = np.argwhere(np.array(mat) == 'S')[0]
+    path = {}
+    start, dir = first(np.array(mat), sx, sy)
+    path[(start[0], start[1])] = (mat[start[0]][start[1]], dir)
+    queue = []
+    queue.append((start[0], start[1]))
+    curr = (start[0], start[1])
+    flag = False
+    while curr != start or not flag:
+        flag = True
+        if curr == (sx,sy): break
+        act = (curr[0] + directions[pipe_directions[path[curr]]][0],
+               curr[1] + directions[pipe_directions[path[curr]]][1])
+        path[act] = (mat[act[0]][act[1]], pipe_directions[path[curr]])
+        curr = act
+    for p in ['|','-','7','L','F','J']:
+        if (p,path[curr][1]) in pipe_directions.keys():
+            if pipe_directions[(p,path[curr][1])] == path[start][1]:
+                new_tpl = (p, path[curr][1])
+                path[curr] = new_tpl
+                return path
+
+print(ex1())
+print(ex2())
